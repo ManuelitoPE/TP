6fb18794aad8142a04052b3bef6bfe473c189a3f
ManuelitoPE <a20210646@pucp.edu.pe>
2023-10-17 17:13:38 -0500

    Practica03 2022.2

---
+++ 2022.2/Practicas/Practica_03/source/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+#include"../header/funciones.h"
+#define MAX_USUARIOS 50
+// int main(int argc, char** argv){
+    int main(){
+    //ARREGLOS
+    int ARRpaisUsuarios[MAX_USUARIOS],ARRcodigoUsuarios[MAX_USUARIOS],
+        ARRseguidoresUsuarios[MAX_USUARIOS];
+    char ARRnombreUsuario[MAX_USUARIOS][20];
+    //ARREGLOS PARA MI REPORTE
+    
+    //
+    //Cargar datos de usuarios
+
+
+    //Ordenar los datos
+    //Emitir reporte
+
+    return 0;
+}
