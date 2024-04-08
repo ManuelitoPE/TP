70d78b57e323130ea0fe1c5e2d7eafdfe22ce0b8
MAKO <a20210646@pucp.edu.pe>
2024-04-08 13:05:28 -0500

    xd

--- 2023.2/Laboratorios/Lab01/header/funciones.h
+++ 2023.2/Laboratorios/Lab01/header/funciones.h
@@ -1,17 +1,21 @@
-// Author: Mako
+//Author: MAKO
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 void emitirReporte();
 void encabezado();
-void linea(char signo);
-void determinacionSexo(char sexo);
-void determinacionCategoria(int anio, int hora_p, int min_p,
-                            int seg_p, int hora_a, int min_a,
-                            int seg_a, char sexo, int &cant_mujeres,
-                            int &cant_menor);
-void determinacionDuracion(int hora_p, int min_p, int seg_p,
-                           int hora_a, int min_a, int seg_a,
-                           char sexo, int anio, int &cant_hombres);
-void determinarCanales(int &cant_personas);
-void determinarCompania(char compania);
+void linea(int n,char c);
+void impresionSexoCategoria(char sexo, int anio);
+void impresionHoraDuracion(int horaI,int minI,int segI,int horaF,
+                        int minF,int segF);
+int convertirSegundos(int hora,int min,int seg);
+void convertirHMS(int& hora,int& min,int& seg,double duracion);
+void lecturaCanales(int canal1,int canal2,int canal3);
+void impresionCompania(char compania);
+void estadisticas(int televidentes);
+void condicionales(int cantMenores,int cantMujAdult,int cantHomJove,
+                   int cantCanales,double promeMenoAdul,double porcentaMujeres,
+                   double porcentajeHombres,double porcentajeAdultos,
+                   double promedioTelevidentes,int dni,int aa,int horaI,
+                   int minI,int segI,int horaF,int minF,int segF,char sexo,
+                   char compania);
 #endif
--- 2023.2/Laboratorios/Lab01/main.cpp
+++ 2023.2/Laboratorios/Lab01/main.cpp
@@ -1,10 +1,9 @@
-// Author: Mako
+// Author: MAKO
 #include <iostream>
 #include <iomanip>
 using namespace std;
 #include "header/funciones.h"
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
     emitirReporte();
     return 0;
 }
