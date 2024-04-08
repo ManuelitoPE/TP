35eee1383a18e452089e5df6dc5da83d76277a4e
MAKO <a20210646@pucp.edu.pe>
2024-04-08 16:34:38 -0500

    Completando Funciones.cpp y agregando estadisticas

--- 2023.2/Laboratorios/Lab01/funciones.cpp
+++ 2023.2/Laboratorios/Lab01/funciones.cpp
@@ -5,42 +5,54 @@ using namespace std;
 #include "header/funciones.h"
 #define MAX_LINEA 110
 void emitirReporte(){
-    cout.precision(4);
+    cout.precision(2);
     cout<<fixed;
     //Variables
     int dni,num,aa,horaI,minI,segI,horaF,minF,segF,canal1,canal2,canal3;
     char c,sexo,compania;
-    // Variables 
+    //Variables
     int televidentes=0,cantMenores=0,cantMujAdult=0,cantHomJove=0,
         cantCanales=0;
+    int cantMenoAdult=0,personasTVPUCP=0,mujeTVPUCP=0,
+        personasTPVIsion=0, hombJoveTPVision=0,adultos=0,canales=0;
     double promeMenoAdul,porcentaMujeres,porcentajeHombres,
-           porcentajeAdultos,promedioTelevidentes;
+           porcentajeAdultos,promedioTelevidentes,duracionH,
+           duracionTotal=0;
+    int hora,min,ss;
     encabezado();
     while(true){
-        lecturaDeDatos();
-        if(cin.eof());
-
-    }
-    while (true){
-        cin>>dni;
+        lecturaDeDatos(dni,aa,horaI,minI,segI,horaF,minF,segF,
+                       canal1,canal2,canal3,sexo,compania,
+                       televidentes);
         if(cin.eof())break;
-        televidentes++;
         cout<<setw(10)<<dni;
-        cin>>sexo>>num>>c>>num>>c>>aa;
         impresionSexoCategoria(sexo,aa);
-        cin>>horaI>>c>>minI>>c>>segI>>horaF>>c>>minF>>c>>segF;
-        impresionHoraDuracion(horaI,minI,segI,horaF,minF,segF);
+        impresionHoraDuracion(horaI,minI,segI,horaF,minF,segF,duracionH);
         lecturaCanales(canal1,canal2,canal3);
-        cin>>compania;
         impresionCompania(compania);
         condicionales(cantMenores,cantMujAdult,cantHomJove,
-                      cantCanales,promeMenoAdul,porcentaMujeres,
-                      porcentajeHombres,porcentajeAdultos,
-                      promedioTelevidentes,dni,aa,horaI,minI,
-                      segI,horaF,minF,segF,sexo,compania);
+                      cantCanales,cantMenoAdult,duracionTotal,personasTVPUCP,
+                      mujeTVPUCP,personasTPVIsion,hombJoveTPVision,
+                      adultos,canales, dni, aa, horaI,
+                      minI, segI, horaF, minF, segF,sexo,
+                      compania,duracionH,canal1,canal2,canal3);
     }
     linea(MAX_LINEA,'=');
-    estadisticas(televidentes);
+    //Calculo Tiempo Promedio que ven los menores y adultos
+    duracionTotal*= 3600;
+    duracionTotal/=cantMenoAdult;
+    convertirHMS(hora,min,ss,duracionTotal);
+    //Porcentaje de mujeres que TVPUcp
+    porcentaMujeres= (mujeTVPUCP*100)/personasTVPUCP;
+    //Porcentaje de Hombres Jovenes que ve TPVision
+    porcentajeHombres=(hombJoveTPVision*100)/personasTPVIsion;
+    //Porcentaje de adultos
+    porcentajeAdultos= (adultos*100)/televidentes;
+    //Promedio de canales que ven los televidentes
+    promedioTelevidentes= (double)canales/televidentes;
+    estadisticas(televidentes,cantMenores,cantMujAdult,cantHomJove,
+                cantCanales,hora,min,ss,porcentaMujeres,
+                porcentajeHombres,porcentajeAdultos,promedioTelevidentes);
 }
 void encabezado(){
     cout<<right<<setw(50)<<"EMPRESA DE OPINION DE MERCADO S.A."<<endl;
@@ -64,12 +76,25 @@ void linea(int n,char c){
     }
     cout<<endl;
 }
+void lecturaDeDatos(int& dni,int& aa,int& horaI,int& minI,
+               int& segI,int& horaF,int& minF,int& segF,
+               int& canal1,int& canal2,int& canal3,char& sexo,
+               char& compania,int& televidentes){
+    char c;
+    int num;
+    cin>>dni;
+    if(cin.eof())return;
+    televidentes++;
+    cin>>sexo>>num>>c>>num>>c>>aa>>horaI>>c>>minI>>c>>segI
+       >>horaF>>c>>minF>>c>>segF>>canal1>>canal2>>canal3
+       >>compania;
+}
 void impresionSexoCategoria(char sexo, int anio){
     // Sexo
     cout<<setw(12);
     if(sexo == 'M'){
         cout<<"Masculino";
-    }else{
+    }else {
         cout<<"Femenino";
     }
     // Categoria
@@ -83,21 +108,21 @@ void impresionSexoCategoria(char sexo, int anio){
     }
 }
 void impresionHoraDuracion(int horaI,int minI,int segI,int horaF,
-                        int minF,int segF){
+                        int minF,int segF,double& duracionH){
     // Hora(A) y Hora(P)
-    cout<<setfill('0')<<setw(2)<<horaI<<':'<<setw(2)<<minI<<':'<<setw(2)<<segI<<setfill(' ')
+    cout<<right<<setfill('0')<<setw(2)<<horaI<<':'<<setw(2)<<minI<<':'<<setw(2)<<segI<<setfill(' ')
         <<setw(3)<<" "<<setfill('0')<<setw(2)<<horaF<<':'<<setw(2)<<minF<<':'<<setw(2)<<segF<<setfill(' ')
-        <<setw(3)<<" ";
+        <<setw(5)<<" ";
     // Duracion
     int horaP = convertirSegundos(horaI,minI,segI);
     int horaA = convertirSegundos(horaF,minF,segF);
     int duracion = horaA - horaP;
-    double duracionH = (double)duracion/3600;
+    duracionH = (double)duracion/3600;
     int hora,min,seg;
     convertirHMS(hora,min,seg,duracion);
 
     cout<<setfill('0')<<setw(2)<<hora<<':'<<setw(2)<<min<<':'<<setw(2)<<seg
-        <<setfill(' ')<<setw(11)<<" "<<duracionH<<setw(8)<<" "<<setfill(' ');
+        <<setfill(' ')<<left<<setw(11)<<" "<<duracionH<<setw(6)<<" "<<setfill(' ');
 }
 int convertirSegundos(int hora,int min,int seg){
     int total = hora*3600+min*60+seg;
@@ -112,16 +137,16 @@ void convertirHMS(int& hora,int& min,int& seg,double duracion){
     seg = duracion;
 }
 void lecturaCanales(int canal1,int canal2,int canal3){
-    int aux=0,cont=0;
-    for (int i = 0; i < 3; i++){
-        cin>>aux;
-        if(aux!=0){
-            if(i!=0)cout<<'/';
-            cout<<aux;
-            cont++;
-        }
-    }
-    cout<<setw(14-cont*4)<<"";
+    cout<<canal1;
+    if(canal2!=0){
+        cout<<'/'<<canal2;
+        if(canal3!=0){
+            cout<<'/'<<canal3;
+            cout<<setw(2)<<" ";
+        }else
+            cout<<setw(6)<<" ";
+    }else
+        cout<<setw(10)<<" ";
 }
 void impresionCompania(char compania){
     if(compania=='A') cout<<"TVPUCP";
@@ -129,25 +154,59 @@ void impresionCompania(char compania){
     else cout<<"TPVision";
     cout<<endl;
 }
-void estadisticas(int televidentes){
+void condicionales(int& cantMenores,int& cantMujAdult,int& cantHomJove,
+                   int& cantCanales,int& cantMenoAdult,double& duracionTotal,
+                   int& personasTVPUCP,
+                   int&  mujeTVPUCP,int& personasTPVIsion,int& hombJoveTPVision,
+                   int& adultos,int& canales,int dni,int aa,int horaI,
+                   int minI,int segI,int horaF,int minF,int segF,char sexo,
+                   char compania,double duracionH,int canal1,int canal2,
+                   int canal3){
+    if(aa>2009 and horaI>=14 and horaF<=19)cantMenores++;
+    if(aa<=1988 and sexo=='F' and ((horaF*100)+minF<930 or (horaI*100)+minI>1245))cantMujAdult++;
+    if(aa<2009 and aa>1988 and sexo=='M' and duracionH>3.5)cantHomJove++;
+    if(canal1!=555 and canal1!=314 and canal2!=555 and canal2!=314 and
+       canal1!=555 and canal1!=314){
+        if(canal1==770 or canal1==312 or canal1==577 or
+            canal2==770 or canal2==312 or canal2==577 or
+            canal3==770 or canal3==312 or canal3==577)cantCanales++;
+    }
+    if(aa>2009 or aa<1988){
+        cantMenoAdult++;
+        duracionTotal+=duracionH;
+    }
+    if(compania=='A'){
+        personasTVPUCP++;
+        if(sexo=='F')mujeTVPUCP++;
+    }
+    if(compania=='C'){
+        personasTPVIsion++;
+        if(sexo=='M' and aa>1988 and aa>2006)hombJoveTPVision++;
+    }
+    if(aa<1988)adultos++;
+    if(canal1!=0){
+        canales++;
+        if(canal2!=0){
+            canales++;
+            if(canal3!=0)canales++;
+        }
+    }
+}
+void estadisticas(int televidentes,int cantMenores, int cantMujAdult,
+                  int cantHombreJove,int cantCanales,int hora,int min,
+                  int ss,double porcentaMujeres,double porcentajeHombres,
+                  double porcentajeAdultos,double promedioTelevidentes){
     cout<<right<<setw(30)<<"ESTADISTICAS OBTENIDAS:"<<endl<<left
         <<setw(7)<<" "<<setw(90)<<"Cantidad de televidentes registrados"<<televidentes<<endl
-        <<setw(7)<<" "<<setw(90)<<"Cantidad de menores que ven entre las 14:00:00 y las 19:00:00"<<endl
-        <<setw(7)<<" "<<setw(90)<<"Cantidad de mujeres adultas que NO ven television entre las 9:30:00 y las 12:45"<<endl
-        <<setw(7)<<" "<<setw(90)<<"Cantidad de hombres jovenes que ve mas de 3 horas y media"<<endl
-        <<setw(7)<<" "<<setw(90)<<"Cantidad de personas que ven los canales 770,312 o 577 pero no 314 ni 555"<<endl
-        <<setw(7)<<" "<<setw(90)<<"Tiempo promedio que ven los menores y adultos"<<endl
-        <<setw(7)<<" "<<setw(90)<<"Porcentaje de mujeres que ve TVPUCP"<<endl
-        <<setw(7)<<" "<<setw(90)<<"Porcentaje de hombres jovenes que ve TPVision"<<endl
-        <<setw(7)<<" "<<setw(90)<<"Porcentaje de adultos Registrados"<<endl
-        <<setw(7)<<" "<<setw(90)<<"Promedio de canales que ven los televidentes"<<endl;
+        <<setw(7)<<" "<<setw(90)<<"Cantidad de menores que ven entre las 14:00:00 y las 19:00:00"<<cantMenores<<endl
+        <<setw(7)<<" "<<setw(90)<<"Cantidad de mujeres adultas que NO ven television entre las 9:30:00 y las 12:45"<<cantMujAdult<<endl
+        <<setw(7)<<" "<<setw(90)<<"Cantidad de hombres jovenes que ve mas de 3 horas y media"<<cantMenores<<endl
+        <<setw(7)<<" "<<setw(90)<<"Cantidad de personas que ven los canales 770,312 o 577 pero no 314 ni 555"<<cantCanales<<endl
+        <<setw(7)<<" "<<setw(90)<<"Tiempo promedio que ven los menores y adultos"
+        <<right<<setfill('0')<<setw(2)<<hora<<':'<<setw(2)<<min<<":"<<setw(2)<<ss<<endl<<setfill(' ')<<left
+        <<setw(7)<<" "<<setw(90)<<"Porcentaje de mujeres que ve TVPUCP"<<porcentaMujeres<<'%'<<endl
+        <<setw(7)<<" "<<setw(90)<<"Porcentaje de hombres jovenes que ve TPVision"<<porcentajeHombres<<'%'<<endl
+        <<setw(7)<<" "<<setw(90)<<"Porcentaje de adultos Registrados"<<porcentajeAdultos<<'%'<<endl
+        <<setw(7)<<" "<<setw(90)<<"Promedio de canales que ven los televidentes"<<promedioTelevidentes<<endl;
     linea(MAX_LINEA,'=');
-}
-void condicionales(int cantMenores,int cantMujAdult,int cantHomJove,
-                   int cantCanales,double promeMenoAdul,double porcentaMujeres,
-                   double porcentajeHombres,double porcentajeAdultos,
-                   double promedioTelevidentes,int dni,int aa,int horaI,
-                   int minI,int segI,int horaF,int minF,int segF,char sexo,
-                   char compania){
-
 }
--- 2023.2/Laboratorios/Lab01/header/funciones.h
+++ 2023.2/Laboratorios/Lab01/header/funciones.h
@@ -3,19 +3,28 @@
 #define FUNCIONES_H
 void emitirReporte();
 void encabezado();
+void lecturaDeDatos(int& dni,int& aa,int& horaI,int& minI,
+               int& segI,int& horaF,int& minF,int& segF,
+               int& canal1,int& canal2,int& canal3,char& sexo,
+               char& compania,int& televidentes);
 void linea(int n,char c);
 void impresionSexoCategoria(char sexo, int anio);
 void impresionHoraDuracion(int horaI,int minI,int segI,int horaF,
-                        int minF,int segF);
+                        int minF,int segF,double& duracionH);
 int convertirSegundos(int hora,int min,int seg);
 void convertirHMS(int& hora,int& min,int& seg,double duracion);
 void lecturaCanales(int canal1,int canal2,int canal3);
 void impresionCompania(char compania);
-void estadisticas(int televidentes);
-void condicionales(int cantMenores,int cantMujAdult,int cantHomJove,
-                   int cantCanales,double promeMenoAdul,double porcentaMujeres,
-                   double porcentajeHombres,double porcentajeAdultos,
-                   double promedioTelevidentes,int dni,int aa,int horaI,
+void condicionales(int& cantMenores,int& cantMujAdult,int& cantHomJove,
+                   int& cantCanales,int& cantMenoAdult,double& duracionTotal,
+                   int& personasTVPUCP,
+                   int&  mujeTVPUCP,int& personasTPVIsion,int& hombJoveTPVision,
+                   int& adultos,int& canales,int dni,int aa,int horaI,
                    int minI,int segI,int horaF,int minF,int segF,char sexo,
-                   char compania);
+                   char compania,double duracionH,int canal1,int canal2,
+                   int canal3);
+void estadisticas(int televidentes,int cantMenores, int cantMujAdult,
+                  int cantHombreJove,int cantCanales,int hora,int min,
+                  int ss,double porcentaMujeres,double porcentajeHombres,
+                  double porcentajeAdultos,double promedioTelevidentes);
 #endif
