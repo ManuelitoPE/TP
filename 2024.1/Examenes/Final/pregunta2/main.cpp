7ce9aa0cb5058257b6d09229bfd6de0187ff78cb
MAKO <a20210646@pucp.edu.pe>
2024-07-08 18:48:30 -0500

    Actualizando Elementos

--- 2024.1/Examenes/Final/pregunta2/main.cpp
+++ 2024.1/Examenes/Final/pregunta2/main.cpp
@@ -1,10 +1,11 @@
 //Author: MAKO
 #include <iostream>
 #include <iomanip>
-#include <fstream>
-#include <cstring>
 using namespace std;
 #include "header/funciones.h"
-#define MAX_LINEA 120
-#define NO_ENCONTRADO -1
 
+int main(){
+        
+
+    return 0;
+}
